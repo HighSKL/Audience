74d509155a19ba082be80bbc6d25b920ea9deaae
HighSKL <kostirko02@gmail.com>
2023-03-02 01:20:03 +0700

    correctNavMenuDesign

--- src/Assets/Common/NavigationMenu/NavigationMenu.tsx
+++ src/Assets/Common/NavigationMenu/NavigationMenu.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../../style/NavigationMenu_style.scss'
 import { VscSignOut } from "react-icons/vsc";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineCalendar, AiOutlineTeam } from "react-icons/ai";
+import { FaUserFriends } from "react-icons/fa";
 
 
 const NavigationMenu = () => {
@@ -9,8 +10,8 @@ const NavigationMenu = () => {
         <div className="wrapper_nav">
             <div className="nav-items">
                 <div className="icon-block"><AiOutlineUser className="icon"/><p>Профиль</p></div>
-                <div className="icon-block"><AiOutlineUser className="icon"/><p>Профиль</p></div>
-                <div className="icon-block"><AiOutlineUser className="icon"/><p>Профиль</p></div>
+                <div className="icon-block"><AiOutlineTeam className="icon"/><p>Команда</p></div>
+                <div className="icon-block"><AiOutlineCalendar className="icon"/><p>Бронь</p></div>
                 <div className="icon-block sign-out-block"><VscSignOut className="sign-out icon"/><p>Выход</p></div>
             </div>
         </div>
